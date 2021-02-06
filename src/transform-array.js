54046a1be9ed9aa82818011594dd2a69e35beff9
vladdominator <73780858+vladdominator@users.noreply.github.com>
2021-02-07 01:47:09 +0300

    feat: add hanoi-tower, what-season

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
 };
