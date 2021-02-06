c31440c0f5e5486d5517e0859c1540a0ea4eec91
vladdominator <73780858+vladdominator@users.noreply.github.com>
2021-02-06 03:34:02 +0300

    feat: add carbon-dating count-cats dream-team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,14 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+   if(typeof sampleActivity !== 'number'){
+      return false;
+   }
+   if(typeof +sampleActivity != NaN){
+      return  false;
+   }
+   const carbonYear = 0.693 / HALF_LIFE_PERIOD;
+   const Year = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / carbonYear);
+   return Year;
 };
